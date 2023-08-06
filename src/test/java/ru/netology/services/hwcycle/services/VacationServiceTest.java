010cd4ec409e0bdea758ceb30dec30fd2666039a
Livenskaya Olga <livenskaya@list.ru>
2023-08-07 00:33:56 +0300

    Add CsvFileSource

--- src/test/java/ru/netology/services/hwcycle/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/hwcycle/services/VacationServiceTest.java
@@ -1,15 +1,35 @@
 package ru.netology.services.hwcycle.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void test (){
-        VacationService service=new
-                
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
 
-        Assertions.assertEquals();
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+    public void testPrimer1 (int expected, int income, int expenses, int threshold){
+        VacationService service=new VacationService();
+
+        //int expected=3;
+        int actual= service.calcVacation(income, expected,threshold);
+
+       // Assertions.assertEquals(expected,actual);
 
     }
+
+// @Test
+//  public void testPrimer2 (){
+//      VacationService service=new VacationService();
+//        int expected=2;
+//        int actual= service.calcVacation(100_000, 60_000, 150_000);
+
+//        Assertions.assertEquals(expected,actual);
+//    }
 }
